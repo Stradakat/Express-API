6fde2f720e187bf346586202d3003bd372476e42
Rick Felix <Felirick@gmail.com>
2026-01-19 14:58:46 -0800

    added JWT

---
+++ backend/controllers/userController.js
@@ -0,0 +1,77 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/userModel');
+const asyncHandler = require('express-async-handler');
+
+// Generate JWT Token
+const generateToken = (id) => {
+    if (!process.env.JWT_SECRET) {
+        throw new Error('JWT_SECRET is not defined in environment variables');
+    }
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    });
+};
+
+//@desc Register a new user
+//@route POST /api/users
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+
+    const { name, email, password } = req.body;
+
+    if(!name || !email || !password) {
+        res.status(400);
+        throw new Error('Please add all fields');
+    }
+// Check if user exists
+    const userExists = await User.findOne({ email });
+    if(userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+
+});
+
+
+//@desc Authenticate a user
+//@route POST /api/users/login
+//@access Public
+const loginUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Login User' });
+});
+
+
+//@desc Get user data
+//@route GET /api/users/me
+//@access Public
+const getMe = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: ' User Data Display' });
+});
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe,
+}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const colors = require('colors');
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv").config({ path: require('path').resolve(__dirname, '../.env') });
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || process.env.PORT || 5000;
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server running on port ${port}`));
