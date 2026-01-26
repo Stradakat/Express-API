d0b0f0639aad4996eaab1c7fc78db188d395f46a
Rick Felix <Felirick@gmail.com>
2026-01-26 15:32:17 -0800

    added json web token functionallity CRUD

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,11 @@
 const asyncHandler = require('express-async-handler');
 const Goal = require('../models/goalModel');
+const User = require('../models/userModel');
 //@desc Get goals
 //@route GET /api/goals
 //@access Private
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find();
+    const goals = await Goal.find({ user: req.user.id });
     res.status(200).json(goals);
 });
 
@@ -19,6 +20,7 @@ const setGoal = asyncHandler(async (req, res) => {
 
     const goal = await Goal.create({
         text: req.body.text,
+        user: req.user.id,
     });
     
     res.status(200).json(goal);
@@ -29,11 +31,24 @@ const setGoal = asyncHandler(async (req, res) => {
 //@access Private
 const updateGoal = asyncHandler(async (req, res) => {
 
-    const goal = await Goal.findById(req.params.id, req.body, { new: true });
+    const goal = await Goal.findById(req.params.id);
 
     if(!goal) {
         res.status(400);
         throw new Error('Goal not found');
+    }
+
+    const user = await User.findById(req.user.id);
+    // Check for user
+    if(!user) {
+        res.status(400);
+        throw new Error('User not found');
+    }
+
+    // Check for goal
+    if(goal.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('User not authorized');
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true });
@@ -52,7 +67,20 @@ const deleteGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found');
     }
 
-    await goal.remove();
+    const user = await User.findById(req.user.id);
+    // Check for user
+    if(!user) {
+        res.status(400);
+        throw new Error('User not found');
+    }
+
+    // Check for goal
+    if(goal.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('User not authorized');
+    }
+
+    await Goal.findByIdAndDelete(req.params.id);
 
     res.status(200).json({ id: req.params.id });
 });
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,15 +3,7 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/userModel');
 const asyncHandler = require('express-async-handler');
 
-// Generate JWT Token
-const generateToken = (id) => {
-    if (!process.env.JWT_SECRET) {
-        throw new Error('JWT_SECRET is not defined in environment variables');
-    }
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '30d',
-    });
-};
+
 
 //@desc Register a new user
 //@route POST /api/users
@@ -59,15 +51,49 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/login
 //@access Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Login User' });
+
+    const { email, password } = req.body;
+    if(!email || !password) {
+        res.status(400);
+        throw new Error('Please add all fields');
+    }
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if(user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid credentials');
+    }
 });
+
+
+// Generate JWT Token
+const generateToken = (id) => {
+    if (!process.env.JWT_SECRET) {
+        throw new Error('JWT_SECRET is not defined in environment variables');
+    }
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    });
+};
 
 
 //@desc Get user data
 //@route GET /api/users/me
-//@access Public
+//@access Private
 const getMe = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: ' User Data Display' });
+    const { _id, name, email } = await User.findById(req.user._id);
+    res.status(200).json({
+        id: _id,
+        name,
+        email,
+    });
 });
 
 module.exports = {
